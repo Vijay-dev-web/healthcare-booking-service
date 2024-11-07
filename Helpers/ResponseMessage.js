46b20ff4dbf2c31f7777f7d2dd5b9bbd10b27304
prakash k <78136945+prakashDoss@users.noreply.github.com>
2024-11-08 00:50:46 +0530

    Code cleanup
    Code cleanup

--- Helpers/ResponseMessage.js
+++ Helpers/ResponseMessage.js
@@ -3,7 +3,5 @@ module.exports = {
     "INTERNAL_SERVER_ERROR": 'Internal server error Or Invalid data',
     "REQUIRED_FIELDS_MISSING": 'Required fields missing',
     "DATA_NOT_FOUND":'Data not found',
-    "AUTH_TOKEN_MISSING": 'Token Missing',
-    "CACHE_MESSAGE_SUCCESS": 'The collection has been successfully saved to the Redis cache!.',
-    "CACHE_MESSAGE_DELETED": 'The collection has been successfully deleted to the Redis cache!.'
+    "AUTH_TOKEN_MISSING": 'Token Missing'
 }
