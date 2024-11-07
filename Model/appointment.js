1d383369f5b11513410130668a57319d0e9d8ceb
prakash k <78136945+prakashDoss@users.noreply.github.com>
2024-11-08 00:47:35 +0530

    Update appointment.js
    Update appointment.js

--- Model/appointment.js
+++ Model/appointment.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose');
-
 const appointmentSchema = new mongoose.Schema({
-  user_id: { type: String, required: true },
-  doctor_id: { type: String, required: true },
-  appointmentDate: { type: Date, required: true },
-  appointmentTime: { type: String, required: true },
-  reason: { type: String, required: true },
-  notes: { type: String, required: true },
-  active_status: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now }
+    user_id: { type: String, required: true },
+    doctor_id: { type: String, required: true },
+    patient_name: { type: String, required: true },
+    doctor_name: { type: String, required: true },
+    appointmentDate: { type: Date, required: true },
+    appointmentTime: { type: String, required: true },
+    reason: { type: String, required: true },
+    notes: { type: String, required: true },
+    active_status: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now }
 });
 
 const Appointment = mongoose.model('Appointment', appointmentSchema);
